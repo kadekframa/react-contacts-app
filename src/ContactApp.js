b6977a2b08c122158ffe6b289d13ce7c1fcec999
kadekframa <dframa19@gmail.com>
2022-05-28 13:01:42 +0800

    React Contacts App. Displayed Contact List. Keep Spirit !!!

---
+++ src/ContactApp.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ContactList from "./ContactList";
+import { getData } from "./data";
+
+const ContactApp = () => {
+    const contacts = getData();
+
+    return (
+        <div className="contact-app">
+            <h1>Daftar Kontak</h1>
+            <ContactList contacts={contacts} />
+        </div>
+    );
+}
+
+export default ContactApp;
---
+++ src/ContactList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContactItem from "./ContactItem";
+
+const ContactList = ({ contacts }) => {
+    return (
+        <div className="contact-list">
+            {contacts.map((contact) => 
+            <ContactItem {...contact} key={contact.id} />
+        )}
+        </div>
+    );
+}
+
+export default ContactList;
